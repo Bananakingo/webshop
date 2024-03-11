d5d7689e30cd449a7c44df16d7f9f4f38381e9c2
Bananakingo <vandenbosch2003@gmail.com>
2024-03-11 22:07:07 +0100

    pushing basis for front end

---
+++ sandingdisks.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'API/Controllers/customer.controller.php';
+
+// Only give errors
+error_reporting(E_ERROR | E_PARSE);
+
+// Set up header page
+$page_title = "Bosch Abrasives";
+$page_subtitle = "Schuurmiddelen voor elke toepassing";
+$active = 1;
+ 
+include 'html\header.html';
+
+//getAllCustomers( setConnection() );
+
+//getCustomer( setConnection(), 3) ;
+
+
+
+?>
